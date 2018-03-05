da1988a075b79465405f66df631c2deb657fc37f
halenv <halenv@rose-hulman.edu>
2018-03-05 09:49:48 -0500

    Finished Module 6

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -5,7 +5,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and Noelle Hale.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
@@ -27,3 +27,41 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+felix = rg.SimpleTurtle('turtle')
+felix.speed = 20
+felix.pen = rg.Pen('orange',10)
+einstein = rg.SimpleTurtle()
+einstein.speed = 30
+einstein.pen = rg.Pen('midnight blue', 5)
+
+sizef = 150
+radius = 30
+
+for k in range (10):
+    felix.draw_regular_polygon(10-k,sizef)
+
+    felix.pen_up()
+    felix.right(45)
+    felix.backward(15)
+    felix.pen_down()
+
+    sizef = sizef - 10
+    felix.speed = 20 + 2*k
+
+for j in range (14):
+
+    einstein.begin_fill()
+    einstein.draw_circle(radius)
+
+    einstein.pen_up()
+    einstein.forward(radius/2)
+    einstein.pen_down()
+
+    radius= radius + 10
+
+window.close_on_mouse_click()
