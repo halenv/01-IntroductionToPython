f17b57a403031c62deae026ccfd90c8afec8c201
halenv <halenv@rose-hulman.edu>
2018-03-05 09:31:22 -0500

    Finished Module 5

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Noelle Hale.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -26,4 +26,21 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
-###############################################################################
+################################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+felix = rg.SimpleTurtle('turtle')
+felix.pen = rg.Pen('red', 10)
+felix.speed = 20
+felix.forward(200)
+felix.right(90)
+felix.forward(200)
+felix.right(45)
+felix.forward(10)
+
+
+
+window.close_on_mouse_click()
